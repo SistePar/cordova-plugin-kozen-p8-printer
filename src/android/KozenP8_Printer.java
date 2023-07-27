153c57a66291376f8bccc5b95668639b9971ee36
Robert <r2galeano@gmail.com>
2023-07-27 17:08:16 -0400

    fixes

--- src/android/KozenP8_Printer.java
+++ src/android/KozenP8_Printer.java
@@ -1,4 +1,4 @@
-package com.sistepar.cordova.KozenP8.Printer;
+package com.sistepar.cordova.kozenp8.printer;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
@@ -6,7 +6,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import android.context.Context;
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.util.Log;
@@ -15,7 +15,7 @@ public class KozenP8_Printer extends CordovaPlugin {
 
     private static final String LOG_TAG = "KozenP8_Printer";
     
-    public boolean execute(String action, JSONArray args, final CallbackContext callback) throws JSONException {
+    public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
         if (action.equals("state")) {
 
             callbackContext.success("ok");
